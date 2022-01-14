f0167c30ffd423180f2a0783da9f146356c1aecf
user <user@DESKTOP-0E1JB75>
2022-01-14 20:55:27 +0900

    Practice01 #3 finish

--- practice01/src/main/java/prob3/Prob3.java
+++ practice01/src/main/java/prob3/Prob3.java
@@ -5,11 +5,28 @@ import java.util.Scanner;
 public class Prob3 {
 	
 	public static void main(String[] args) {
-
 		Scanner scanner = new Scanner(System.in);
+		int num;
+		int sum = 0;
 
-		/* 코드 작성 */
+		System.out.print("숫자를 입력하세요 : ");
+		num = scanner.nextInt();
 		
+		if (num % 2 == 0) {
+			for (int i = 1; i <= num; i++) {
+				if (i % 2 == 0) {
+					sum += i;
+				}
+			}
+		} else {
+			for (int i = 1; i <= num; i++) {
+				if (i % 2 == 1) {
+					sum += i;
+				}
+			}
+		}
+		
+		System.out.println("결과 값 : " + sum);
 		scanner.close();
 	}
 }
