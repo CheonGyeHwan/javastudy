eddb28e75ddf84f6b51015650005afd01752b46e
user <user@DESKTOP-0E1JB75>
2022-01-14 20:57:24 +0900

    Practice01 #4 finish

--- practice01/src/main/java/prob4/Prob4.java
+++ practice01/src/main/java/prob4/Prob4.java
@@ -9,8 +9,17 @@ public class Prob4 {
 
 		System.out.print("문자열을 입력하세요 : ");
 		String text = scanner.nextLine();
-
+		String[] textArr = text.split("");
 		
+		int cnt = 0;
+		for (int i = 0; i < textArr.length; i++) {
+			for (int j = 0; j <= cnt; j++) {
+				System.out.print(textArr[j]);
+			}
+			System.out.print("\n");
+			cnt++;
+		}
+
 		scanner.close();
 	}
 
