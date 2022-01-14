9bdb25db680cd7bb58142c1a4707c9dad34100c2
user <user@DESKTOP-0E1JB75>
2022-01-14 21:16:56 +0900

    Practice02 #2 finish

--- practice02/src/main/java/prob2/Prob2.java
+++ practice02/src/main/java/prob2/Prob2.java
@@ -10,13 +10,17 @@ public class Prob2 {
 		int[] intArray = new int[ 5 ];
 		double sum = 0;
 
-		/* 키보드에서 배열 크기만큼 입력 받아 배열에 저장하는 코드 */
+		System.out.println("5개의 숫자를 입력하세요.");
+		for (int i = 0; i < intArray.length; i++) {
+			intArray[i] = scanner.nextInt();
+		}
 		
-		/* 배열에 저장된 정수 값 더하기 */
+		for (int num : intArray) {
+			sum += num;
+		}
 		
-		/* 출력 */
+		System.out.printf("평균은 %.1f 입니다.\n", sum/(intArray.length));
 		
-		/* 자원정리 */
 		scanner.close();
 	}
 
