5df644f17115b793ce0163fe955ec3c7967a5d3a
user <user@DESKTOP-0E1JB75>
2022-01-14 21:15:01 +0900

    Practice02 #1 finish

--- practice02/src/main/java/prob1/Prob1.java
+++ practice02/src/main/java/prob1/Prob1.java
@@ -6,10 +6,16 @@ public class Prob1 {
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in  );
-
+		int total;
 		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
 
-		/* 코드 작성 */
+		System.out.print("금액 : ");
+		total = scanner.nextInt();
+		
+		for (int money : MONEYS) {
+			System.out.printf("%d원 : %d개\n", money, total/money);
+			total = total%money;
+		}
 		
 		scanner.close();
  	}
