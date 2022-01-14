a0f4f44352501355cf1c63febc6d038f23484566
user <user@DESKTOP-0E1JB75>
2022-01-14 21:12:58 +0900

    Practice02 #Init Commit

---
+++ practice02/src/main/java/prob1/Prob1.java
@@ -0,0 +1,17 @@
+package prob1;
+
+import java.util.Scanner;
+
+public class Prob1 {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner( System.in  );
+
+		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
+
+		/* 코드 작성 */
+		
+		scanner.close();
+ 	}
+
+}
