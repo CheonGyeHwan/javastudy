88208fda28cf81b57a5105b5c1a90fc820990c8d
user <user@DESKTOP-0E1JB75>
2022-01-17 20:10:21 +0900

    Practice03 #4 finish

--- practice03/src/main/java/prob4/StringUtil.java
+++ practice03/src/main/java/prob4/StringUtil.java
@@ -1,5 +1,14 @@
 package prob4;
 
 public class StringUtil {
+	
+	public static String concatenate(String[] str) {
+		String result = "";
+		
+		for (String value : str) {
+			result += value;
+		}
+		return result;
+	}
 
 }
