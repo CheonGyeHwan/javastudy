5b03c9df652cf62047bf28a6d9679f4659dbf590
user <user@DESKTOP-0E1JB75>
2022-01-17 19:21:27 +0900

    Practice02 #4 finish

--- practice02/src/main/java/prob4/Prob4.java
+++ practice02/src/main/java/prob4/Prob4.java
@@ -11,12 +11,17 @@ public class Prob4 {
 	}
 	
 	public static char[] reverse(String str) {
-		/* 코드를 완성합니다 */
-		return null;
+		char[] result = new char[str.length()];
+		
+		for(int i = 0; i < str.length(); i++) {
+			int idx = str.length() - i - 1;
+			result[idx] = str.charAt(i);
+		}
+		
+		return result;
 	}
 
 	public static void printCharArray(char[] array){
-		/* 코드를 완성합니다 */
 		System.out.println( array );
 	}
 
