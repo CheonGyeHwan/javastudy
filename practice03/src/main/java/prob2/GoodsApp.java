f91fa13ee4e46f20ee7867386234c7a3abfcc7b5
user <user@DESKTOP-0E1JB75>
2022-01-17 20:00:47 +0900

    Practice03 #2 finish

--- practice03/src/main/java/prob2/GoodsApp.java
+++ practice03/src/main/java/prob2/GoodsApp.java
@@ -11,8 +11,25 @@ public class GoodsApp {
 		Goods[] goods = new Goods[COUNT_GOODS];
 
 		// 상품 입력
+		Goods item = null;
+		for(int i = 0; i < goods.length; i++) {
+			String input = scanner.nextLine();
+			String[] inputArr = input.split(" ");
+			
+			item = new Goods();
+			item.setName(inputArr[0]);
+			item.setPrice(Integer.parseInt(inputArr[1]));
+			item.setCnt(Integer.parseInt(inputArr[2]));
+			
+			goods[i] = item;
+		}
+		
+		System.out.println();
 
 		// 상품 출
+		for(Goods tmp : goods) {
+			System.out.printf("%s(가격: %d원)이 %d개 입고 되었습니다.\n", tmp.getName(), tmp.getPrice(), tmp.getCnt());
+		}
 		
 		// 자원정리
 		scanner.close();
