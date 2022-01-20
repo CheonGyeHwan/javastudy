ac9914bff7c50e8791445521e9a283a439e671e6
user <user@DESKTOP-0E1JB75>
2022-01-20 21:12:59 +0900

    Practice05 #5 진행중

--- practice05/src/main/java/prob5/MyStack.java
+++ practice05/src/main/java/prob5/MyStack.java
@@ -1,5 +1,67 @@
 package prob5;
 
 public class MyStack {
+	private String[] stack;
+	
+	public MyStack(int length) {
+		stack = new String[length];
+	}
+	
+	public void push(String inputVal) {
+		
+		if (stack[stack.length - 1] == null) {
+			for (int i = 0; i < stack.length; i++) {
+				if (stack[i] == null) {
+					stack[i] = inputVal;
+					break ;
+				}
+			}
+		} else {
+			String[] newStack = new String[stack.length + 1];
+			for (int i = 0; i < stack.length; i++) {
+				newStack[i] = stack[i];
+			}
+			newStack[newStack.length - 1] = inputVal;
+			
+			
+			stack = new String[newStack.length];
+			for (int i = 0; i < stack.length; i++) {
+				stack[i] = newStack[i];
+			}
+			
+			newStack = null;
+		}
+	}
+	
+	public String pop() {
+		String popVal;
+		
+		if (stack.length == 1) {
+			popVal = stack[0];
+			stack[0] = null;
+		} else {
+			popVal = stack[stack.length - 1];
+			
+			String[] newStack = new String[stack.length - 1];
+			for (int i = 0; i < newStack.length; i++) {
+				newStack[i] = stack[i];
+			}
+			
+			stack = new String[newStack.length];
+			for (int i = 0; i < stack.length; i++) {
+				stack[i] = newStack[i];
+			}
+		}
+		return popVal;
+	}
+	
+	public boolean isEmpty() {
+		
+		if (stack[0] == null ) {
+			return true;
+		} else {
+			return false;
+		}	
+	}
 
 }
---
+++ practice05/src/main/java/prob5/MyStackException.java
@@ -0,0 +1,13 @@
+package prob5;
+
+public class MyStackException extends RuntimeException {
+	
+	public MyStackException() {
+		
+	}
+	
+	public MyStackException(String message) {
+		super(message);
+	}
+	
+}
