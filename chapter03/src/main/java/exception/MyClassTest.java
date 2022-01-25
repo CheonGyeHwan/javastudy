c445330bdc276ffe44bffbc6a84298498dfb6b02
user <user@DESKTOP-0E1JB75>
2022-01-25 10:18:03 +0900

    ExceptionTest

---
+++ chapter03/src/main/java/exception/MyClassTest.java
@@ -0,0 +1,16 @@
+package exception;
+
+public class MyClassTest {
+
+	public static void main(String[] args) {
+		try {
+			MyClass mc = new MyClass();
+			mc.danger();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("exit");
+	}
+
+}
